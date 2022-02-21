a73f80542c03651d747a342716b165d9a72beb79
Robert Dobyns <robertddobyns@gmail.com>
2022-02-20 20:24:00 -0600

    fix header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,28 +1,54 @@
 import React from 'react';
-import {Box, Typography} from "@mui/material";
+import {styled, Box, Typography} from "@mui/material";
 
-const headerStyle = {
-  height: '100px',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  flexDirection: 'column',
-  paddingTop: '20px',
-  margin: '10px',
-  marginBottom: '50px'
-}
+const Root = styled('div')(({theme}) => ({
+  margin: '0 auto',
+  textAlign: 'center',
+  marginBottom: '30px',
+  [theme.breakpoints.down('sm')]: {
+    backgroundColor: 'red'
+  }
+}))
+
+const TitleContainer = styled('div')(({theme}) => ({
+  fontWeight: 'bold',
+  margin: '20px',
+  [theme.breakpoints.down('sm')]: {
+    fontSize: '25px'
+  },
+  [theme.breakpoints.up('sm')]: {
+    fontSize: '40px'
+  },
+  [theme.breakpoints.up('md')]: {
+    fontSize: '50px'
+  }
+}));
+
+const SubtitleContainer = styled('div')(({theme}) => ({
+  textAlign: 'center',
+  paddingBottom: '10px',
+  [theme.breakpoints.down('sm')]: {
+    fontSize: '15px'
+  },
+  [theme.breakpoints.up('sm')]: {
+    fontSize: '20px'
+  },
+  [theme.breakpoints.up('md')]: {
+    fontSize: '30px'
+  }
+}));
 
 const Header = () => {
-  
-  
   return (
-    <Box>
-      <Box sx={headerStyle}>
-        <Typography variant={'h3'} component={'h1'} gutterBottom>Angela's Literary Speakeasy</Typography>
-        <Typography variant={'subtitle1'}>An ever growing list of books banned in schools and communities across the US</Typography>
-        <Typography variant={'subtitle2'}>The checkboxes are only there for your use right now. I do not want any of your personal information so I do not save them.</Typography>
-      </Box>
-    </Box>
+    <Root>
+      <TitleContainer>
+        Angela's Literary Speakeasy
+      </TitleContainer>
+      <SubtitleContainer>
+        An ever growing list of books banned in schools and communities across the United States
+      </SubtitleContainer>
+      <Typography variant={'subtitle2'} sx={{textAlign: 'center'}}>The checkboxes are only there for your use right now. I do not want any of your personal information so I do not save them.</Typography>
+    </Root>
   )
   
 }
