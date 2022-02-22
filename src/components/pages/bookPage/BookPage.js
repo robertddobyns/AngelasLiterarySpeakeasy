9a4e57e3772cdc640930335757dc8b251c20f6a8
Robert Dobyns <robertddobyns@gmail.com>
2022-02-21 22:55:45 -0600

    fixed BookPage formatting

--- src/components/pages/bookPage/BookPage.js
+++ src/components/pages/bookPage/BookPage.js
@@ -7,8 +7,15 @@ import SchoolInfo from "./SchoolInfo";
 const Root = styled('div')(({theme}) =>({
   maxWidth: '1000px',
   margin: '0 auto',
-  minHeight: '60vh'
+  minHeight: '60vh',
+  padding: '0 10px'
 }))
+
+const BannedListContainer = styled('div')(({theme}) => ({
+  [theme.breakpoints.down('sm')] : {
+  
+  }
+}));
 
 const BookPage = () => {
   const [page, setPage] = useState([]);
@@ -26,8 +33,8 @@ const BookPage = () => {
   return (
     <Root>
       <Typography variant={'h3'} component={'h1'} sx={{textAlign: 'center', fontStyle: 'italic'}}>{page.name}</Typography>
-      <Typography variant={'h5'} sx={{textAlign: 'center'}}>By: {page.author}</Typography>
-      <Box>
+      <Typography variant={'h5'} sx={{textAlign: 'center', marginBottom: '30px'}}>By: {page.author}</Typography>
+      <BannedListContainer>
         {page.locations?.length > 0 && <Typography variant={'h4'}>Banned Locations</Typography>}
         {page.locations?.map(item => (
           <SchoolInfo
@@ -37,7 +44,7 @@ const BookPage = () => {
             reasons={item.reasons}
             year={item.year}
           />))}
-      </Box>
+      </BannedListContainer>
     </Root>
   )
   
